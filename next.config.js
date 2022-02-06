cada5aec33e5a4d92972a3029653f51b2138526d
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-06 15:29:06 +0900

    Feat: API_KEY masking

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+const API_KEY = process.env.API;
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    // url 이동
+    return [
+      {
+        source: "/old-page/:path*", // source에 입력한 url에 있을 시
+        destination: "new-page/:path*", // destination에 입력한 url로 이동
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const API_KEY = "7235027e37c8e65ba10fcb25022b2073";
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
       const {
         data: { results },
-      } = await axios.get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-      );
+      } = await axios.get("/api/movies");
       setMovies(results);
     })();
   }, []);
